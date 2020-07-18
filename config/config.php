c7d29cb51f44ac718888ce9eaf738253c85ffb20
lou <53594864+ashlyzette@users.noreply.github.com>
2020-07-17 17:15:52 -0700

    change database address

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    $con = mysqli_connect('localhost','root','','cpatests');
+    $con = mysqli_connect("162.241.24.185","apexhris_loam","8v9Xmnb!$1iaDc6Z","apexhris_amigo");
     if (mysqli_connect_error()){
         echo 'failed to connect to database' . mysqli_connect_errno();
     }
