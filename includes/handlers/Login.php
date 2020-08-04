a187c49702612e3a9ce2b66fc31950848b44cbd2
lou <53594864+ashlyzette@users.noreply.github.com>
2020-08-04 13:21:49 -0700

    deny access if verification is not completed

--- includes/handlers/Login.php
+++ includes/handlers/Login.php
@@ -3,14 +3,18 @@
         //Sanitize email then save email session
         $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
         $_SESSION['email'] = $email;
-        $login_obj = mysqli_query($con, "SELECT first_name, email, password FROM users WHERE email = '$email'");
+        $login_obj = mysqli_query($con, "SELECT first_name, email, verified password FROM users WHERE email = '$email'");
 
         if ($login = mysqli_fetch_array($login_obj)){
             $verify = password_verify($_POST['password'],$login['password']);
             if ($verify){
-                $_SESSION['user']= $login['first_name'];
-                header("Location: index.php");
-                exit();
+                if ($login['verified']== 'yes'){
+                    $_SESSION['user']= $login['first_name'];
+                    header("Location: index.php");
+                    exit();
+                } else {
+                    array_push($error_array, "Please verify your email address to gain access to test banks");
+                }
             } else  {
                 array_push($error_array, "Incorrect email and password, please try again!");
             }
